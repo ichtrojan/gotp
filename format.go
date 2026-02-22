ea42408403124bbf15d592ed13b6917791d9b772
Trojan <ordrizzy@gmail.com>
2026-02-22 16:13:54 +0000

    follow Go naming conventions for format constants

--- format.go
+++ format.go
@@ -1,9 +1,18 @@
 package gotp
 
-type format int
+type Format int
 
 const (
-	ALPHA format = iota
-	ALPHA_NUMERIC
-	NUMERIC
+	Alpha Format = iota
+	AlphaNumeric
+	Numeric
 )
+
+// Deprecated: ALPHA is deprecated, use Alpha instead.
+const ALPHA = Alpha
+
+// Deprecated: ALPHA_NUMERIC is deprecated, use AlphaNumeric instead.
+const ALPHA_NUMERIC = AlphaNumeric
+
+// Deprecated: NUMERIC is deprecated, use Numeric instead.
+const NUMERIC = Numeric
